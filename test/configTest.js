3ac54548afd4c7edd434f64cc3fbbbce2f8f789f
Florian Merz <flomerz@gmail.com>
2018-07-17 13:57:13 +0200

    Clean up config test.

--- test/configTest.js
+++ test/configTest.js
@@ -1,9 +1,13 @@
+import { promisify } from "util";
+import fs from "fs";
+
 import { validateConfig, load } from "../lib/config";
 import { EMPTY } from "./configs";
 
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import mock from "mock-fs";
+import tmp from "tmp";
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -32,12 +36,20 @@ describe("everything configy", () => {
       return async () => (typeof cfg === "string" ? cfg : JSON.stringify(cfg));
     }
 
-    it("validate valid config", () => {
-      return load("", mkConfig(EMPTY)).should.eventually.be.deep.equal(EMPTY);
+    it("validate valid config", done => {
+      tmp.file((_, path, fd) =>
+        promisify(fs.write)(fd, JSON.stringify(EMPTY))
+          .then(() => load(path).should.eventually.be.deep.equal(EMPTY))
+          .then(() => done())
+      );
     });
 
-    it("error on empty config", () => {
-      return load("", mkConfig({})).should.be.rejectedWith(/missing/);
+    it("error on empty config", done => {
+      tmp.file((_, path, fd) =>
+        promisify(fs.write)(fd, JSON.stringify({}))
+          .then(() => load(path).should.throw)
+          .then(() => done())
+      );
     });
   });
 });
